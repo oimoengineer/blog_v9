37cdaa774576c80023c3db637e5928c35a369a64
oimoengineer <oimoengineer0224@gmail.com>
2022-09-16 23:48:57 +0900

    delete修正

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -67,6 +67,8 @@
           'use strict';
           if(confirm('削除すると復元できません。\n本当に削除しますか？')){
               document.getElementById('form_delete').submit();
+          } else {
+            return false;
           }
       }
       </script>
